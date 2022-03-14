810f9dbbe19f7bed35171dd98c3910d21bdb7b76
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-15 07:50:15 +0900

    Replace png with svg

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    package_data={'pyqt_comic_viewer.ico': ['book.svg', 'close.png']},
+    package_data={'pyqt_comic_viewer.ico': ['book.svg', 'close.svg']},
     description='PyQt Comic Viewer',
     url='https://github.com/yjg30737/pyqt-comic-viewer.git',
     install_requires=[
